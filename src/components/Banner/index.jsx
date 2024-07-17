91f0e39693779f6740f8aa84340c51bc7ffb5844
fernando-hess <dhofher.hess@gmail.com>
2024-07-17 01:41:00 -0600

    Actualiza destacado

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -3,7 +3,9 @@ import styles from "./Banner.module.css"
 
 function Banner({img, color}) {
     return(
-        <div className={styles.capa} style={{backgroundImage: `url("/img/banner-${img}.png")`}}>
+        <div className={styles.capa} 
+         style={{backgroundImage: `url("/img/banner-${img}.png")`}}
+        >
             <div className={styles.gradient} style={{background: `${color}`}}>
                 <Destacado />
             </div>
--- src/components/Destacado/Destacado.jsx
+++ src/components/Destacado/Destacado.jsx
@@ -27,8 +27,8 @@
 
 
 
-import React from "react";
-import { useEffect, useState } from "react";
+
+import React, { useEffect, useState, useRef } from "react";
 import Slider from "react-slick";
 import Titulo from "../Titulo";
 import styles from "./Destacado.module.css";
@@ -63,14 +63,19 @@ const PrevArrow = (props) => {
 
 const Destacado = () => {
   const [videos, setVideos] = useState([]);
+  const [activeSlide, setActiveSlide] = useState(0);
+  const videoRefs = useRef([]);
 
   useEffect(() => {
     const fetchVideos = async () => {
       try {
-        const response = await fetch("https://66927881346eeafcf46d06de.mockapi.io/api/v1/videos");
+        const response = await fetch(
+          "https://66927881346eeafcf46d06de.mockapi.io/api/v1/videos"
+        );
         const data = await response.json();
-        console.log("Data fetched:", data); // Verificar aquí los datos
-        setVideos(data);
+        // Aleatorizar el arreglo de videos
+        const shuffledVideos = shuffleArray(data);
+        setVideos(shuffledVideos);
       } catch (error) {
         console.error("Error fetching data: ", error);
       }
@@ -81,12 +86,18 @@ const Destacado = () => {
 
   const convertToEmbedLink = (url) => {
     const urlObj = new URL(url);
-    if (urlObj.hostname === "www.youtube.com" && urlObj.pathname === "/embed") {
+    if (
+      urlObj.hostname === "www.youtube.com" &&
+      urlObj.pathname === "/embed"
+    ) {
       return url;
-    } else if (urlObj.hostname === "www.youtube.com" && urlObj.pathname === "/watch") {
+    } else if (
+      urlObj.hostname === "www.youtube.com" &&
+      urlObj.pathname === "/watch"
+    ) {
       const videoId = urlObj.searchParams.get("v");
       if (videoId) {
-        return `https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1`;
+        return `https://www.youtube.com/embed/${videoId}?autoplay=0`;
       }
     }
     return url;
@@ -100,17 +111,43 @@ const Destacado = () => {
     slidesToScroll: 1,
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
+    beforeChange: (oldIndex, newIndex) => {
+      if (videoRefs.current[oldIndex]) {
+        videoRefs.current[oldIndex].contentWindow.postMessage(
+          '{"event":"command","func":"pauseVideo","args":""}',
+          "*"
+        );
+      }
+    },
+    afterChange: (index) => {
+      setActiveSlide(index); // Actualiza el slide activo
+    },
+  };
+
+  // Función para aleatorizar un arreglo
+  const shuffleArray = (array) => {
+    let currentIndex = array.length, randomIndex;
+  
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
   };
 
   return (
     <section className={styles.destacado}>
       <p className={styles.destacados}>DESTACADOS:</p>
       <Slider {...settings}>
-        {videos.map((video) => (
+        {videos.map((video, index) => (
           <div key={video.id}>
             <div className={styles.descripcionVideo}>
               <div className={styles.tituloContainer}>
-                <Titulo color="#2271D1">
+                <Titulo color="#083369">
                   <h1>{video.categoria}</h1>
                 </Titulo>
               </div>
@@ -118,16 +155,20 @@ const Destacado = () => {
               <p>{video.descripcion}</p>
             </div>
             <div className={styles.videoDestacado}>
-              <iframe
-                width="100%"
-                height="100%"
-                src={convertToEmbedLink(video.link)}
-                title={video.titulo}
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerPolicy="strict-origin-when-cross-origin"
-                allowFullScreen
-              ></iframe>
+              {/* Renderiza el iframe solo si es el slide activo */}
+              {activeSlide === index && (
+                <iframe
+                  ref={(el) => (videoRefs.current[index] = el)}
+                  width="100%"
+                  height="100%"
+                  src={convertToEmbedLink(video.link)}
+                  title={video.titulo}
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerPolicy="strict-origin-when-cross-origin"
+                  allowFullScreen
+                ></iframe>
+              )}
             </div>
           </div>
         ))}
@@ -137,6 +178,22 @@ const Destacado = () => {
 };
 
 export default Destacado;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
